59b054bc95a2c392d907baf7306bd794d1921c4a
Though0123 <mustafayev6111@gmail.com>
2024-08-19 15:30:43 +0500

    U

--- OOP1.py
+++ OOP1.py
@@ -5,8 +5,11 @@ os.system('clear')
 def summm(x,y):
     return x+y
 
+
+
 print(summm(10, 10))
 
 class Animal:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,x,y) -> None:
+        self.x = x
+        self.y = y

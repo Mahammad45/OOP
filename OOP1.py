3be5ece037861c3f8999d4c0b31beb256c2d65b0
Though0123 <mustafayev6111@gmail.com>
2024-08-19 17:07:27 +0500

    U

--- OOP1.py
+++ OOP1.py
@@ -11,7 +11,7 @@ os.system('clear')
 # float | decimal or FLoat   2.00 2.0000000000
 
 # def name():
-# model Name(): or model Name:
+# brand Name(): or brand Name:
 
 # def summm(x, y):
 #     r = x + y
@@ -21,7 +21,7 @@ os.system('clear')
 #     return r
 
 
-# model Animal:
+# brand Animal:
 
     # Класс Animal представляет простую модель животного с двумя параметрами `x` и `y`. 
     # Класс предоставляет методы для сложения и умножения этих параметров.
@@ -63,7 +63,7 @@ os.system('clear')
 #         return self.x * self.y
     
 #     def __str__(self) -> str:
-#         return "model Animal"
+#         return "brand Animal"
 
 
 # a = Animal(10, 10)
@@ -74,9 +74,9 @@ os.system('clear')
 # print(a.umnogenniya())
 # print(a)
 
-# model A:
+# brand A:
 #     def name(self):
-#         return "model name 'A'"
+#         return "brand name 'A'"
     
 # b = A()
 # print(b.name())
@@ -85,11 +85,11 @@ os.system('clear')
 # ''.replace('l', 'k')
 
 
-# model Cat:
-#     def __init__(self, name, age,color,tip) -> None:
+# brand Cat:
+#     def __init__(self, name, age,price,tip) -> None:
 #         self.name = name
 #         self.age = age
-#         self.color = color
+#         self.price = price
 #         self.tip = tip
         
 
@@ -103,7 +103,7 @@ os.system('clear')
 #         return f"{self.name} is meowing"
 
 #     def __str__(self) -> str:
-#         return f"{self.name} {self.age} {self.color} {self.tip}"
+#         return f"{self.name} {self.age} {self.price} {self.tip}"
 
 # cat1 = Cat("Barsik", 3, "black", "persian")
 # cat2 = Cat("Murzik", 5, "white", "siamese")
@@ -119,37 +119,84 @@ os.system('clear')
 
 
 
-class Car:
-    def __init__(self, make, model,year,color) -> None:
-        self.make = make
-        self.model = model
-        self.year = year
-        self.color = color
+# class Car:
+#     def __init__(self, name, brand,category,price) -> None:
+#         self.name = name
+#         self.brand = brand
+#         self.category = category
+#         self.price = price
+        
+
+#     def ready(self):
+#         return f"{self.name} is ready"
+
+#     def wait(self):
+#         return f"{self.name} is wait"
+
+#     def stop(self):
+#         return f"{self.name} is stop"
+    
+#     def start(self):
+#         return f"{self.name} is start"
+
+#     def __str__(self) -> str:
+#         return f"{self.name} {self.brand} {self.category} {self.price}"
+
+# car1 = Car("BMW", 3, "black", 2016) 
+# car2 = Car("Mercedes", 5, "white", 2019) 
+# car2 = Car("Toyota", 5, "black", 2020) 
+# car3 = Car("Ford", 2, "gray", 2005)
+# car4 = Car("Chevrolet", 4, "black",2014)
+
+# print(car1)
+# print(car4.ready())
+# print(car2.wait())
+# print(car3.stop())
+
+
+
+
+class Product:
+    def __init__(self, name, brand,category,price,description,calories,fat,carbohydrates,protein,fiber) -> None:
+        self.name = name
+        self.brand = brand
+        self.category = category
+        self.price = price
+        self.description = description
+        self.calories = calories
+        self.fat = fat
+        self.carbohydrates = carbohydrates
+        self.protein = protein
+        self.fiber = fiber
+
         
 
     def ready(self):
-        return f"{self.make} is ready"
+        return f"{self.name} is old"
 
     def wait(self):
-        return f"{self.make} is wait"
+        return f"{self.name} is new"
 
     def stop(self):
-        return f"{self.make} is stop"
+        return f"{self.name} is finish"
     
     def start(self):
-        return f"{self.make} is start"
+        return f"{self.name} is last 1"
 
     def __str__(self) -> str:
-        return f"{self.make} {self.model} {self.year} {self.color}"
+        return f"{self.name} {self.brand} {self.category} {self.price} {self.description} {self.calories}{self.fat} {self.carbohydrates} {self.protein} {self.fiber}"
 
-car1 = Car("BMW", 3, "black", 2016) 
-car2 = Car("Mercedes", 5, "white", 2019) 
-car2 = Car("Toyota", 5, "black", 2020) 
-car3 = Car("Ford", 2, "gray", 2005)
-car4 = Car("Chevrolet", 4, "black",2014)
+ 
+product1 = Product("Milk", "MilkCompany", "milk", 2.5, "milk for human", 100, 3, 5, 2, 0)
+product2 = Product("Bread", "BreadCompany", "bread", 1.5, "bread for human", 150, 2, 10, 3, 1)
+product3 = Product("Eggs", "EggsCompany", "eggs", 3.0, "eggs for human", 200, 4, 8, 6, 0)
+product4 = Product("Cheese", "CheeseCompany", "cheese", 4.0, "cheese for human", 250, 6, 4, 8, 2)
+product5 = Product("Butter", "ButterCompany", "butter", 5.0, "butter for human", 300, 8, 2, 10, 0)
 
-print(car1)
-print(car4.ready())
-print(car2.wait())
-print(car3.stop())
+
+print(product1)
+print(product4.ready())
+print(product2.wait())
+print(product3.stop())
+
 

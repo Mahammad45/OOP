e7f7d176a3e4cbe5c9d440f17ac20fb6a2151327
Though0123 <mustafayev6111@gmail.com>
2024-08-20 15:28:14 +0500

    UU

--- OOP2.py
+++ OOP2.py
@@ -4,15 +4,31 @@ import sys
 os.sysyem('clear')
 
 # Инкапсуляция
-# public
-# protect _
-# private __
+# public      | общественный
+# protect _    | защищать
+# private __    | частный
 
 
-class Tefal:
+class Teapot:
     def __init__(self,name,year,color) -> None:
         self.name = name
         self._year = year
         self.__color = color
 
+    def on(self):
+        return(f'{self.name} is on')
+    def off(self):
+        return(f'{self.name} is off')
     
+    def heating(self):
+        return(f'{self.name} is heating')
+    
+    def __str__(self) -> str:
+        return(f'{self.name} {self._year} {self.__color}')
+    
+
+    Teapot = Teapot('LG', 2020, 'black')
+    Teapot1= Teapot('BEKO', 2019, 'white')
+    Teapot2= Teapot('SAMSUNG', 2018, 'red')
+    Teapot3= Teapot('TERMAX', 2017, 'blue')
+    Teapot4= Teapot('Teapot', 2016, 'green')

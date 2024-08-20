abf26b9d44efe2b3890eeac80ce568e4f3ad657a
Though0123 <mustafayev6111@gmail.com>
2024-08-20 15:19:34 +0500

    UU

--- OOP2.py
+++ OOP2.py
@@ -2,3 +2,17 @@ import os
 import sys
 
 os.sysyem('clear')
+
+# Инкапсуляция
+# public
+# protect _
+# private __
+
+
+class Tefal:
+    def __init__(self,name,year,color) -> None:
+        self.name = name
+        self._year = year
+        self.__color = color
+
+    
